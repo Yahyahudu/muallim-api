61e88d654d3c24fa3ded6446341c11196fecdd8b
Yahaya Hudu <huduyahaya001@gmail.com>
2026-05-09 20:32:39 +0100

    fix composer install

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Services\SearchIntelligence;
 
-Route::get('/', function () {return view('maintenace');});
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'gell'
+    ]);
+});
 
 Route::get('/tafsir', [TafsirController::class, 'index']);
 Route::get('/tafsir/{id}', [TafsirController::class, 'show'])->where('id', '[a-zA-Z0-9_\-]+');
